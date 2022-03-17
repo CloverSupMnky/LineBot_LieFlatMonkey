8a17f0f555198810c4f91984b9cc2eaf2e8ee394
cloversupmnky <cloversupmnky@gmail.com>
2022-03-17 16:03:16 +0800

    更新 Entity

---
+++ 3.Entities/LineBot_LieFlatMonkey.Entities/Contexts/LineBotLieFlatMonkeyContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using LineBot_LieFlatMonkey.Entities.Models;
+
+namespace LineBot_LieFlatMonkey.Entities.Contexts
+{
+    public partial class LineBotLieFlatMonkeyContext : DbContext
+    {
+        public virtual DbSet<TarotCard> TarotCards { get; set; }
+
+        public LineBotLieFlatMonkeyContext(DbContextOptions<LineBotLieFlatMonkeyContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TarotCard>(entity =>
+            {
+                entity.HasKey(e => e.SeqNo)
+                    .HasName("TarotCard_pkey");
+
+                entity.ToTable("TarotCard");
+
+                entity.HasComment("塔羅牌資料表");
+
+                entity.Property(e => e.SeqNo)
+                    .HasComment("流水號")
+                    .HasIdentityOptions(null, null, null, 99L, null, null);
+
+                entity.Property(e => e.DescDaily)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .HasColumnName("Desc_Daily")
+                    .HasComment("每日運勢牌意");
+
+                entity.Property(e => e.DescRev)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .HasColumnName("Desc_Rev")
+                    .HasComment("逆位牌意");
+
+                entity.Property(e => e.DescUp)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .HasColumnName("Desc_Up")
+                    .HasComment("正位牌意");
+
+                entity.Property(e => e.ImageUrlRev)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("ImageUrl_Rev")
+                    .HasComment("逆位牌圖片連結");
+
+                entity.Property(e => e.ImageUrlUp)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("ImageUrl_Up")
+                    .HasComment("正位牌圖片連結");
+
+                entity.Property(e => e.Mean)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .HasComment("意義");
+
+                entity.Property(e => e.MeanRev)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .HasColumnName("Mean_Rev")
+                    .HasComment("逆位牌意關鍵字");
+
+                entity.Property(e => e.MeanUp)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .HasColumnName("Mean_Up")
+                    .HasComment("正位牌意關鍵字");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .HasComment("中文名稱");
+
+                entity.Property(e => e.NameEn)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .HasColumnName("Name_EN")
+                    .HasComment("英文名稱");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ 3.Entities/LineBot_LieFlatMonkey.Entities/Models/TarotCard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using URF.Core.EF.Trackable;
+
+namespace LineBot_LieFlatMonkey.Entities.Models
+{
+    /// <summary>
+    /// &#22612;&#32645;&#29260;&#36039;&#26009;&#34920;
+    /// </summary>
+    public partial class TarotCard : Entity
+    {
+        /// <summary>
+        /// 流水號
+        /// </summary>
+        public int SeqNo { get; set; }
+        /// <summary>
+        /// 中文名稱
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 英文名稱
+        /// </summary>
+        public string NameEn { get; set; }
+        /// <summary>
+        /// 意義
+        /// </summary>
+        public string Mean { get; set; }
+        /// <summary>
+        /// 正位牌意
+        /// </summary>
+        public string DescUp { get; set; }
+        /// <summary>
+        /// 逆位牌意
+        /// </summary>
+        public string DescRev { get; set; }
+        /// <summary>
+        /// 正位牌意關鍵字
+        /// </summary>
+        public string MeanUp { get; set; }
+        /// <summary>
+        /// 逆位牌意關鍵字
+        /// </summary>
+        public string MeanRev { get; set; }
+        /// <summary>
+        /// 正位牌圖片連結
+        /// </summary>
+        public string ImageUrlUp { get; set; }
+        /// <summary>
+        /// 逆位牌圖片連結
+        /// </summary>
+        public string ImageUrlRev { get; set; }
+        /// <summary>
+        /// 每日運勢牌意
+        /// </summary>
+        public string DescDaily { get; set; }
+    }
+}
