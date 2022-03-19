4e01ce9ce0a1277ea981698957988bdc081b8079
cloversupmnky <cloversupmnky@gmail.com>
2022-03-19 16:27:35 +0800

    新增 Audio 訊息回傳 Model

---
+++ 2.Assets/LineBot_LieFlatMonkey.Assets/Model/LineBot/AudioResultMessage.cs
@@ -0,0 +1,22 @@
+﻿using LineBot_LieFlatMonkey.Assets.Constant;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineBot_LieFlatMonkey.Assets.Model.LineBot
+{
+    public class AudioResultMessage : ResultMessage
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; } = MessageType.Audio;
+
+        [JsonProperty("originalContentUrl")]
+        public string OriginalContentUrl { get; set; }
+
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+    }
+}
---
+++ 6.WebHost/LineBot_LieFlatMonkey.WebHost/Controllers/EnglishSentenceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace LineBot_LieFlatMonkey.WebHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnglishSentenceController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public IActionResult GetSentence() 
+        {
+            return Ok();
+        }
+    }
+}
